9e2a7804fdc1e666df9a5edf25921ae68e1431fb
AndresF.Daza <andresdaza04@hotmail.com>
2018-12-09 17:31:30 -0500

    Se agrega .gitignore, junto al ej2 class1.6 de POOJS

---
+++ 1.6 folder/script1-6.js
@@ -0,0 +1,25 @@
+// Cliente GYM
+
+const clienteGym = {
+    nombre : "Andres",
+    edad : 20,
+    peso : 66,
+    estatura: 178,
+    fechaRegistro : "09/12/2018"
+}
+
+const clienteGym2 = {
+    nombre : "Felipe",
+    edad : 19,
+    peso : 63,
+    estatura: 172,
+    fechaRegistro : "08/12/2018"
+}
+
+const clienteGym3 = {
+    nombreCompleto : "Nicolas Nanais",
+    edad : 19,
+    fechaDeRegistro : "N/A"
+}
+
+const clientes = []
